60f5c76ee105cc5f918c333ee87353b51c821ba0
Kevia <cloud@kevia.me>
2016-01-26 11:49:25 -0800

    save point for pachinko game

---
+++ PeggleClone/PeggleClone/GameScene.swift
@@ -0,0 +1,55 @@
+//
+//  GameScene.swift
+//  PeggleClone
+//
+//  Created by jhampac on 1/26/16.
+//  Copyright (c) 2016 jhampac. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene
+{
+    override func didMoveToView(view: SKView)
+    {
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .Replace
+        background.zPosition = -1
+        addChild(background)
+        physicsBody = SKPhysicsBody(edgeLoopFromRect: frame)
+        
+        makeBouncerAt(CGPoint(x: 0, y: 0))
+        makeBouncerAt(CGPoint(x: 256, y: 0))
+        makeBouncerAt(CGPoint(x: 512, y: 0))
+        makeBouncerAt(CGPoint(x: 768, y: 0))
+        makeBouncerAt(CGPoint(x: 1024, y: 0))
+        
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        if let touch = touches.first
+        {
+            let location = touch.locationInNode(self)
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            ball.physicsBody!.restitution = 0.8
+            ball.position = location
+            addChild(ball)
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+    
+    func makeBouncerAt(pos: CGPoint)
+    {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = pos
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2.0)
+        bouncer.physicsBody!.dynamic = false
+        addChild(bouncer)
+    }
+}
